de493b4d44a4711024854f6f9990e48771d4dde7
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-09-20 21:54:16 +0200

    Small fix

--- src/com/codecool/snake/Main.java
+++ src/com/codecool/snake/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         game = new Game();
-        primaryStage.setTitle("Snake Game");
+        primaryStage.setTitle("Super Snake");
         primaryStage.setScene(new Scene(game, Globals.WINDOW_WIDTH, Globals.WINDOW_HEIGHT));
         primaryStage.show();
         game.start();
